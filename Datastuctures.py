49d0bc2ef4096b2c1a94cfd2f891bd2ce1f43a62
Your Full Name <your@email.com>
2025-06-02 15:08:00 +0530

    Datastructures

--- Datastuctures.py
+++ Datastuctures.py
@@ -57,7 +57,105 @@ print(grade[:-1])
 names=("chan","mike","chai","leo","lakshman")
 name=list(names)
 name.append("chandana")
-print(name)
+print(name)'''
+
+#Sets
+
+'''collection={4,5,7,8,"hello","world","hello"}
+print(collection)
+print(type(collection))
+print(len(collection))
+
+collection=set() #empty set creation
+print(type(collection))
+
+collection=set()
+collection.add(1)
+collection.add(7) #add element to the set
+collection.add("chandana")
+print(collection)
+print(len(collection))
+
+collection=set()
+collection.add(1)
+collection.add(7) #add element to the set
+collection.add("chandana")
+collection.clear() #empties the set
+print(collection)
+print(len(collection))
+
+collection={"hello","python","world","chandan"}
+print(collection.pop())
+print(collection.pop())
+
+set1={1,4,6}
+set2={4,7,9}
+#union to combine two set and return new value
+print(set1.union(set2))
+#combine common value and return new
+print(set1.intersection(set2))
+
+sub={"python","java","c++","python","javascript","java","python","java","c++","c"}
+print(len(sub))
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 9, 6}
+# Difference: Elements in set1 but not in set2
+result = set1.difference(set2)
+print("Difference (set1 - set2):", result)
+
+set1 = {1, 2, 3, 4}
+set2 = {3, 4, 5, 6}
+# Symmetric Difference: Elements not common to both sets
+result = set1.symmetric_difference(set2)
+print("Symmetric Difference:", result)
+
+#Dictionary
+
+dict={"name":"chandana",
+    "age":21,
+    "bloodgroup":"ab+",
+    "marks":[67,90,78],
+    12:94 }
+print(dict)
+print(type(dict))
+print(dict["name"])
+print(dict["age"])
+dict["surname"]="naik"
+print(dict)
+
+#nested dictionary
+
+student={
+    "name":"chaitanya",
+    "subjects":{
+    "physics":90,
+    "chemistry":89,
+    "math":87
+   }
+ }
+print(student["subjects"]["chemistry"])
+
+student={
+    "name":"chaitanya",
+    "subjects":{
+    "physics":90,
+    "chemistry":89,
+    "math":87
+   }
+ }
+print(student.keys()) #return all key values
+print(list(student.keys()))
+print(len(list(student.keys())))
+#return all the values
+print(student.values())
+#return all (key,value) pairs as tuples
+print(student.items())
+#return the key according to value
+print(student.get("name"))
+#insert the specified value to dictionary
+student.update({"city":"banglore","rollno":78})
+print(student)
 
 
 
